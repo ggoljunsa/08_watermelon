cdd319a8d983d89f726602bc89916193b2657454
quart-cavestory <jangminjun6014@gmail.com>
2023-06-17 01:00:12 +0900

    1시간째
    random 함수 생성
    생성 로직(랜덤) 구현할 것

--- scene/ball.gd
+++ scene/ball.gd
@@ -1,8 +1,19 @@
 extends RigidBody2D
 
+var ball_collision
+var ball_texture
+var ball_Area
+
+
+var ball_level = 0 
+@export var ball_size = [30, 200, 300]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ball_collision = $CollisionShape2D
+	ball_texture = $TextureRect
+	ball_Area = $Area2D/CollisionShape2D
 	pass # Replace with function body.
 
 
@@ -12,4 +23,11 @@ func _process(delta):
 
 
 func _on_area_2d_area_entered(area):
+	
 	pass # Replace with function body.
+
+func ball_setsize(num: int):
+	ball_level = num
+	ball_collision.shape = ball_size[num]
+	ball_Area.shape = ball_size[num] + 30
+	
--- scene/main.gd
+++ scene/main.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
 var ball_object = load("res://scene/ball.tscn")
+var numbers = [0, 1, 2, 3]
+@export var probabilities = [40, 30, 20, 10]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	pass # Replace with function body.
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +19,22 @@ func _process(delta):
 func _unhandled_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		#print("마우스 위치: ", event.position)
-		var instance = ball_object.instantiate()
-		instance.position = event.position
-		add_child(instance)
+		pass
+
+func ball_generate(num, pos):
+	var instance = ball_object.instantiate()
+	instance.position = pos
+	add_child(instance)
+
+func rand_generate():
+	var random_number = randi()%100 # 0 to 99
+	
+	if random_number < probabilities[0]:
+		return 0
+	elif random_number < probabilities[1]+probabilities[0]:
+		return 1
+	elif random_number < probabilities[2]+probabilities[1]+probabilities[0]:
+		return 2
+	else:
+		return 3
+
