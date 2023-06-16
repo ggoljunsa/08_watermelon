e8bef473aa92e38034a974c73d772cfb04b9c3c9
quart-cavestory <jangminjun6014@gmail.com>
2023-06-17 00:34:26 +0900

    first commit
    preload에서 해맸다. instantiate의 차이점을 노션에 기록을 해 놓자.

--- scene/ball.gd
+++ scene/ball.gd
@@ -9,3 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_area_2d_area_entered(area):
+	pass # Replace with function body.
