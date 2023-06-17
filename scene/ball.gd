e489049b6cc7c95a20ecf2c5d797aab969d8e950
quart-cavestory <jangminjun6014@gmail.com>
2023-06-17 23:00:36 +0900

    merge error solve
    call_deferred 쓰자.

--- scene/ball.gd
+++ scene/ball.gd
@@ -11,7 +11,7 @@ var ball_level = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ball_collision = get_node("$CollisionShape2D")
+	ball_collision = get_node("CollisionShape2D")
 	#print($CollisionShape2D.shape.get_radius())
 	ball_texture = $TextureRect
 	ball_Area = get_node("Area2D/CollisionShape2D")
@@ -26,27 +26,43 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	if area.get_parent() is RigidBody2D:
-		var other_ball = area.get_parent()
+		var other_ball = area.get_parent() #other ball will be the slow one, which gets bigger
 		if other_ball.ball_level == ball_level:
-			print("merge")
-			merge_ball(other_ball)
+			if speed(linear_velocity) < speed(other_ball.linear_velocity):
+				set_physics_process(false)
+				merge_ball(other_ball)
+				await get_tree().create_timer(0.1).timeout
+				set_physics_process(true)
+
+func speed(linear_velocity):
+	return sqrt(linear_velocity.x*linear_velocity.x + linear_velocity.y*linear_velocity.y)
 
 func merge_ball(other_ball):
-	other_ball.set_physics_process(false)
-	other_ball.position = position  # Immediately move the other ball to this ball's position
-	other_ball.queue_free()
-	ball_setsize(ball_level + 1)  # Increase the level and update the size
+	set_physics_process(false)
+	position = other_ball.position   # Immediately move the other ball to this ball's position
+	other_ball.ball_setsize(ball_level + 1)  # Increase the level and update the size
+	queue_free()
+
+
 
 
 
 func ball_setsize(num: int):
+	print(num, 'setsize')
+	set_physics_process(false)
 	ball_level = num
-	print(ball_size[num])
 	ball_collision = get_node("CollisionShape2D")
-	ball_collision.shape = CircleShape2D.new()
-	ball_collision.shape.set_radius(float(ball_size[num]))
+
+	var new_shape = CircleShape2D.new()
+	new_shape.set_radius(float(ball_size[num]))
+	ball_collision.call_deferred("set", "shape", new_shape)
 	
 	ball_Area = get_node("Area2D/CollisionShape2D")
-	ball_Area.shape = CircleShape2D.new()
-	ball_Area.shape.set_radius(float(ball_size[num]+collision_radius))
+	
+	var new_area_shape = CircleShape2D.new()
+	new_area_shape.set_radius(float(ball_size[num] + collision_radius))
+	ball_Area.call_deferred("set", "shape", new_area_shape)
+
+	set_physics_process(true)
+
 	
